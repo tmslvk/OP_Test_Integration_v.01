86dca7de332c2ac185a4f0b9ef229f04ed8321cc
Timofey Selyavko <timofey.s@orangeprocess.by>
2026-04-28 17:45:44 +0300

    Добавлена минимальная реализация логирования

---
+++ Schemas/OPCarsBaseIntegrationLogger/OPCarsBaseIntegrationLogger.cs
@@ -0,0 +1,66 @@
+namespace BPMSoft.Configuration.OP1CIntegration.Logger
+{
+    using BPMSoft.Common;
+    using BPMSoft.Configuration.Logger;
+    using BPMSoft.Configuration.OP1CIntegrationModels.Exceptions;
+    using BPMSoft.Core;
+    using System;
+    using System.Collections.Generic;
+
+    internal class OPCarsBaseIntegrationLoggerImpl : OPBaseIntegrationLogger
+    {
+        #region Fields : Public
+
+        public override string SchemaName => "OPCarsBaseIntegrationLog";
+
+        #endregion
+
+        #region Methods : Public
+
+        public override void LogError(UserConnection connection, Guid logId, Exception exception, bool withStackTrace = false)
+        {
+            if (logId.IsEmpty())
+                logId = CreateLog(connection);
+
+            var errorText = $"{exception.Message}";
+            var errorType = exception.GetType().Name;
+            if (exception is RequestProcessingException requestException)
+            {
+                errorText = $"[{requestException.IdCarsBase}]: {requestException.Message}";
+                errorType = requestException.ExceptionType.Name;
+            }
+            if (withStackTrace)
+                errorText += $"\nStackTrace:\n{exception.StackTrace}";
+
+            connection.UpdateEntityById(SchemaName, logId, new Dictionary<string, object>()
+            {
+                { "OPErrorText", errorText },
+                { "OPErrorType", errorType }
+            });
+        }
+
+        #endregion
+    }
+
+    public static class OPCarsBaseIntegrationLogger
+    {
+        #region Fields : Private
+
+        private static readonly OPBaseIntegrationLogger Impl = new OPCarsBaseIntegrationLoggerImpl();
+
+        #endregion
+
+        #region Methods : Public
+
+        public static Guid LogRequest(UserConnection connection, string methodName, object request) =>
+            Impl.LogRequest(connection, methodName, request);
+
+        public static void LogResponse(UserConnection connection, Guid logId, object response) =>
+            Impl.LogResponse(connection, logId, response);
+
+        public static void LogError(UserConnection connection, Guid logId, Exception exception, bool withStackTrace = false) =>
+            Impl.LogError(connection, logId, exception, withStackTrace);
+
+        #endregion
+    }
+} 
---
+++ Schemas/OPConstants/OPConstants.cs
@@ -0,0 +1,11 @@
+ namespace BPMSoft.Configuration.OPConstants
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class OPResponseStatus
+    {
+        public static string OK { get; set; } = "OK";
+        public static string Error { get; set; } = "Error";
+    }
+}
