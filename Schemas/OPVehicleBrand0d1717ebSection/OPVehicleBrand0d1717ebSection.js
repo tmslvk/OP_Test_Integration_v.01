387eb5c91edc95eda86e735244daffa1f064ea6b
dev_pc1 <dev_pc1@orangeprocess.by>
2026-04-28 14:41:56 +0300

    section data added

--- Schemas/OPVehicleBrand0d1717ebSection/OPVehicleBrand0d1717ebSection.js
+++ Schemas/OPVehicleBrand0d1717ebSection/OPVehicleBrand0d1717ebSection.js
@@ -1,8 +1,31 @@
-define("OPVehicleBrand0d1717ebSection", [], function() {
+define("OPVehicleBrand0d1717ebSection", ["ServiceHelper"], function(ServiceHelper) {
 	return {
 		entitySchemaName: "OPVehicleBrand",
 		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"parentName": "SeparateModeActionButtonsContainer",
+				"propertyName": "items",
+				"name": "ImportVehicleButton",
+				"values": {
+					"itemType": BPMSoft.ViewItemType.BUTTON,
+					"caption": {"bindTo": "Resources.Strings.ImportButtonCaption"},
+					"click": {"bindTo": "onImportButtonClick"},
+				}
+			}
+		]/**SCHEMA_DIFF*/,
+		methods: {
+			onImportButtonClick: function()	{
+				
+				var serviceData = {};
+				
+				ServiceHelper.callService("OPVehicleBrandService", "ImportAllBrandsAndModelsAsync",
+                    function(response) {
+                        var result = response.ImportAllBrandsAndModelsAsyncResult;
+                        this.showInformationDialog(result);
+                    }, serviceData, this);
+			}
+		}
 	};
 });
